780da59f4c9cdc926535164498e92b9b3a138d56
enea dhack <enea.so@live.com>
2018-04-01 22:10:48 -0500

    Initial Commit

---
+++ config/authorizationGUI.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // authorizations that will be necessary to manage the administration interface.
+    'authorizations' => [
+        // has total control over the users.
+        'administrator' => 'authorization-administrator',
+
+        // you can only see the roles and permissions that a certain user has.
+        'view' => 'view-authorizations',
+
+        // you can grant roles and permissions but not remove them.
+        'grant' => 'grant-authorizations',
+
+        // you can only revoke roles or permissions.
+        'revoke' => 'revoke-authorizations',
+
+        // You can create new roles or permissions, in addition to assigning permissions to a specific role.
+        'create' => 'create-authorizations',
+    ],
+
+    // routes for administration.
+    'routes' => [
+        'prefix' => [
+            // uri: app.com/administration/authorizations.
+            'main' => 'administration/authorizations',
+            // uri: app.com/administration/authorizations/permissions.
+            'permissions' => 'permissions',
+            // uri: app.com/administration/authorizations/roles.
+            'roles' => 'roles',
+        ],
+    ],
+];
